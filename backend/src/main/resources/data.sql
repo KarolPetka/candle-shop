7574dba6062240a71b0e92716b200e7e417c59ec
karol-petka_swi <karol.petka@solarwinds.com>
2024-10-30 21:07:21 +0100

    remove commented queries and clear user table

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -1,3 +1,5 @@
+DELETE FROM user;
+
 INSERT INTO user (email, password, name, address, phone, role)
 VALUES ('customer@mail.com',
         '$2a$12$RLs1fYOdiUP0VzjvrjnCLehuz/CP9bVuobqVkKjbYqgzPV1cJtY32', -- BCrypt hash for "12345"
@@ -13,13 +15,4 @@ VALUES ('admin@mail.com',
         '123 Main St, Springfield',
         '555-1234',
         'ADMIN');
-
--- INSERT INTO category (id, name, description)
--- VALUES (1, 'Candles', 'Various scented candles');
--- INSERT INTO category (id, name, description)
--- VALUES (2, 'Accessories', 'Accessories for candle care');
---
--- INSERT INTO product (id, name, description, weight, burn_time, price, stock, category_id)
--- VALUES (1, 'Lavender Candle', 'A relaxing lavender-scented candle.', 0.5, 40, 15.99, 100, 1);
-
 
